9a47f10a7a33caf1487e06b1310c8270c26ceddc
Davidchhing <davidhchhing@gmail.com>
2018-05-20 14:53:46 -0700

    fixed constring and joined articles and authors

--- starter-code/server.js
+++ starter-code/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = '';// TODO: Don't forget to set your own conString
+const conString = 'postgres://davidchhing@localhost:5432/kilovolt';// TODONE: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
@@ -23,8 +23,11 @@ app.get('/new', function(request, response) {
 
 app.get('/articles', function(request, response) {
   // REVIEW: This query will join the data together from our tables and send it back to the client.
-  // TODO: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
-  client.query(``)
+  // TODONE: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
+  
+  client.query(`SELECT articles.*, authors.*
+  FROM articles 
+  JOIN ON articles.author_id=authors.author_id`)
   .then(function(result) {
     response.send(result.rows);
   })
